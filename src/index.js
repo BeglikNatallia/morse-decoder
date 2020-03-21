a3665624dcaccd795be0e9d1b40a29c860d498be
Natallia Behlik <natasha-1909@yandex.ru>
2020-03-22 00:03:47 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let divededBy10 = [];
+    let morseLetter = [];
+    let encodedSymbol = {
+        '10': '.',
+        '11': '-',
+        '00': '',
+        '**': ' '
+    };
+
+    for (let i = 0; i < expr.length; i += 10) {
+        divededBy10.push(expr.substring(i, i + 10));
+    }
+
+    for (let i = 0; i < divededBy10.length; i++) {
+        let divededBy2 = [];
+        for (let j = 0; j < divededBy10[i].length; j += 2) {
+            divededBy2.push(divededBy10[i].substring(j, j + 2));
+        }
+        morseLetter.push(divededBy2.map(el => encodedSymbol[el]).join(''));
+    }
+
+    return morseLetter.map(el => {
+            if (el === '     ') return ' ';
+
+            return MORSE_TABLE[el];
+        }
+    ).join('');
+
 }
 
 module.exports = {
     decode
-}
+};
